534672bf9302f1c666519d426257d0e529114068
ChangYuMine <king09372000@gmail.com>
2016-09-26 17:52:40 +0800

    0926_arraytest

---
+++ src/tw/com/google/www_concept/Array.java
@@ -0,0 +1,19 @@
+package tw.com.google.www_concept;
+
+public class Array {
+
+	public static void main(String[] args) {
+		
+		int[][]a=new int[][]{{1,2,3},{4,5,6}};
+		
+		System.out.println(a.length);
+		
+		for(int v1:a[1]){
+	
+			System.out.println(v1);
+			
+		}
+
+	}
+
+}

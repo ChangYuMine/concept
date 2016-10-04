7e0dfeedfa68cd444df0cc84df1bbf1c10ff43e8
ChangYuMine <king09372000@gmail.com>
2016-10-04 15:01:55 +0800

    存取修飾字

---
+++ src/tw/com/google/www_concept/Loadword.java
@@ -0,0 +1,34 @@
+package tw.com.google.www_concept;
+
+//0828_3_32min
+
+/*存取修飾字：
+1.public(開放)
+2.protected(繼承之子類別及相同package可存取)
+3.沒有
+4.private(本類別限制存取)
+ */
+
+public class Loadword {
+
+	public static void main(String[] args) {		
+
+	}
+
+}
+
+class Brad01{
+	
+	void m1(){
+		
+		
+	}
+}
+
+class Brad02 extends Brad01{
+	
+	void m1(){
+		
+		
+	}
+}
